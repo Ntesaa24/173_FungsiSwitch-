82f9b82829d2680e5a618c3fda3531ca7ebc5aae
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-03-26 10:55:10 +0700

    System CLS

--- SwitchCase/SwitchCase.cpp
+++ SwitchCase/SwitchCase.cpp
@@ -34,3 +34,12 @@ int main()
 			do{
 				system("CLS"); 
 				cout << "Menu Kalkulator sederhana" << endl; 
+				cout << "1. Penjumlahan" << endl; 
+				cout << "2. Pengurangan" << endl; 
+				cout << "3. Perkalian" << endl; 
+				cout << "4. Pembagian" << endl; 
+				cout << "5. keluar" << endl; 
+				cout << "Masukan pilihan: "; 
+				cin >> pilihan; 
+
+int main()
